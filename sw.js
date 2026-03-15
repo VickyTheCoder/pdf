b790a1e6d74ac01d65d81c767b2e01aadc1becb8
VickyTheCoder <90819597+VickyTheCoder@users.noreply.github.com>
2026-03-15 16:48:10 +0530

    Fixed to make it work offline

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
-self.addEventListener("install",()=>{})
+const CACHE_NAME = "pdftool-v1"
+
+const urlsToCache = [
+  "/pdf/",
+  "/pdf/index.html",
+  "/pdf/app.js",
+  "/pdf/manifest.json"
+]
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache)
+    })
+  )
+})
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request)
+    })
+  )
+})
